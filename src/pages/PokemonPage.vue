a9266b36fd961cfbd3645134a7e5766edc39eb5f
Yeison Esteban Pretel Puentes <43614257+yeprepue@users.noreply.github.com>
2024-05-14 19:16:52 -0500

    refactor: :tada: Se completa llamada de los nombres aleatorio de los Pokemons
    Se agrego peticion con axios para obtener el nombre de 4 pokemons aleatoria mente en el pokemonsOptions

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,24 +1,35 @@
 <template>
-        <div>
-          <h1>¿Quién es este Pokémon?</h1>
-          <PokemonPicture :pokemonId="649" :showPokemon="true" />
-          <PokemonOptions />
-        </div>
-      </template>
-      
-      <script>
-      import PokemonPicture from '@/components/PokemonPicture';
-      import PokemonOptions from '@/components/PokemonOptions';
-      import getPokemons from '@/helpers/getPokemons';
-      
-      console.log(getPokemons());
-      export default {
-        components: {
-          PokemonPicture,
-          PokemonOptions
-        }
-      }
-      </script>
-      vgy u9op{
-        dfgk._
-      }
+  <div>
+    <h1>¿Quién es este Pokémon?</h1>
+    <PokemonPicture :pokemonId="59" :showPokemon="true" />
+    <PokemonOptions :pokemons="pokemonsArr" />
+  </div>
+</template>
+
+<script>
+import PokemonPicture from "@/components/PokemonPicture.vue";
+import PokemonOptions from "@/components/PokemonOptions.vue";
+import { getPokemonOptions } from "@/herlpers/getPokemonOptions.js";
+
+
+// console.log(getPokemonOptions())
+
+export default {
+  components: { PokemonPicture, PokemonOptions },
+  data() {
+    return {
+      pokemonsArr: [], 
+    };
+  },
+  methods:{
+    async mixPokemonArray() {
+      this.pokemonsArr = await getPokemonOptions()
+      console.log(this.pokemonsArr)
+    }
+  },
+  mounted() {
+    this.mixPokemonArray()
+  }
+};
+</script>
+
